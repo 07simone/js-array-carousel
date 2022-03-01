ac8cd0c0ffabb950742e29ef534370bd1c8c2d39
simone andreozzi <simoneandreozzi8@gmail.com>
2022-03-01 22:04:22 +0100

    inserimento-bottone-incompleto

--- js/main.js
+++ js/main.js
@@ -12,21 +12,57 @@ const items = [
 
 let caroselloContent="";
 for(let i = 0; i < items.length; i++){
-    caroselloContent+=
-    `${items[i]}`
+    caroselloContent +=
+    `<img class="img-dimension"src="${items[i]}" alt="">`
+    
 }
 
+//1)creo elemento img
+//2)imposto src
+//3)la inserisco nell'html 
 
-const caroselloWrapper = document.querySelector("carosello");
-caroselloWrapper.innerHTML += immaginiElements;   
+let immagine = document.createElement("img");
+immagine.src = items[3]; 
+immagine.id = ("immaginePrincipale"); 
+
+const elementContenuto = document.querySelector(".contenuto");
+elementContenuto .appendChild(immagine); 
+
+const caroselloWrapper = document.querySelector(".lista-immagini");
+caroselloWrapper.innerHTML += caroselloContent;     
 
 
-const immaginiElements = document.getElementsByClassName("lista-immagini");
-immaginiElements[0].classList.add("active")
-console.log(immaginiElements);
+/* const immaginiElements = document.getElementsByClassName("lista-immagini");
+immaginiElements[0].classList.add("active") */
+/* console.log(immaginiElements); */ 
 
 
-/* const followingBottom = document.querySelector("following");
-followingBottom.addEventListener("click", function(){
+const followingBottom = document.querySelector(".following");
 
-}) */
+let contatore = 0;
+
+followingBottom.addEventListener("click", function() { 
+
+    caroselloWrapper [contatore] .classList.remove('active');
+    
+    contatore++;
+
+    caroselloWrapper [contatore] .classList.add('active');
+
+
+  /* console.log("ciao")  */ 
+ }); 
+
+/* const previousOneBottom = document.querySelector(".previousOne");
+
+previousOneBottom.addEventListener("click", function() {
+    
+    caroselloWrapper[contatore].classList.remove('active');
+    
+    contatore--;
+
+    caroselloWrapper[contatore].classList.add('active'); 
+
+
+  console.log("ciao")  
+}); */
